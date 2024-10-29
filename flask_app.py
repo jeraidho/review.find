de3c3ac83ad87030db9a7fd1efe7c46f3f07b9a6
tresjuess <113621221+tresjuess@users.noreply.github.com>
2024-10-30 01:48:37 +0300

    correct usage of the class

--- flask_app.py
+++ flask_app.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask import url_for, render_template, request, redirect
 import csv
 import pandas as pd
-from dataset/datalib import load_json_data
-from dataset/datalib import SentenceFinder
+from dataset.datalib import load_json_data
+from dataset.datalib import SentenceFinder
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,11 +19,16 @@ def query_process():
         search = request.form['query']
         query = SentenceFinder(metadata, tokendata, lemma_index_data)
         result = query.process_query(search)
-        with open('result.csv', 'w', newline='', encoding='utf-8') as file:
-            datawriter = csv.DictWriter(file, delimiter='\t', fieldnames=['text_id', 'sentence', 'sentiment', 'film_name'])
-            for sentence in result:
-                datawriter.writerow(sentence)
-        return redirect(url_for('search_result'))
+        if result:
+            with open('result.csv', 'w', newline='', encoding='utf-8') as file:
+                datawriter = csv.DictWriter(file, delimiter='\t', fieldnames=['text_id', 'sentence', 'sentiment', 'film_name'])
+                for sentence in result:
+                    datawriter.writerow(sentence)
+            return redirect(url_for('search_result'))
+        else:
+            with open('result.txt', 'w', encoding='utf-8') as file:
+                file.write('Предложения не найдены')
+            return redirect(url_for('nothing_found'))
     return render_template('search.html')
 
 @app.route('/search_result')
@@ -32,6 +37,13 @@ def output_page():
     out = df.values.tolist()
     out_ = out
     return render_template('search_result.html', out_=out_)
+
+@app.route('/nothing_found')
+def nothing_found():
+    with open('result.txt', 'r', encoding='utf-8') as file:
+        nothing = file.read()
+    out_ = nothing
+    return render_template('nothing_found.html', out_=out_)
 
 if __name__ == '__main__':
     app.run(debug=True)
