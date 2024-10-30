87193341298e46e301604d64fc2003d76b9ef16b
tresjuess <113621221+tresjuess@users.noreply.github.com>
2024-10-30 16:32:48 +0300

    new working version

--- flask_app.py
+++ flask_app.py
@@ -2,23 +2,19 @@ from flask import Flask
 from flask import url_for, render_template, request, redirect
 import csv
 import pandas as pd
-from dataset.datalib import load_json_data
 from dataset.datalib import SentenceFinder
 app = Flask(__name__)
+query = SentenceFinder(inner=True)
 
 @app.route('/')
 def index():
     return render_template('index.html')  # главная страница
 
 @app.route('/search', methods = ['GET', 'POST'])
-def query_process():
-    metadata = load_json_data('metadata.json')
-    tokendata = load_json_data('tokendata.json')
-    lemma_index_data = load_json_data('lemma_index.json')
+def search():
     if request.method == 'POST':
-        search = request.form['query']
-        query = SentenceFinder(metadata, tokendata, lemma_index_data)
-        result = query.process_query(search)
+        srch = request.form['query']
+        result = query.process_query(srch)
         if result:
             with open('result.csv', 'w', newline='', encoding='utf-8') as file:
                 datawriter = csv.DictWriter(file, delimiter='\t', fieldnames=['text_id', 'sentence', 'sentiment', 'film_name'])
@@ -26,24 +22,19 @@ def query_process():
                     datawriter.writerow(sentence)
             return redirect(url_for('search_result'))
         else:
-            with open('result.txt', 'w', encoding='utf-8') as file:
-                file.write('Предложения не найдены')
             return redirect(url_for('nothing_found'))
     return render_template('search.html')
 
-@app.route('/search_result')
-def output_page():
-    df = pd.read_csv('result.csv', encoding='utf-8')
+@app.route('/search_result', methods=['GET', 'POST'])
+def search_result():
+    df = pd.read_csv('result.csv', delimiter='\t', encoding='utf-8')
     out = df.values.tolist()
     out_ = out
     return render_template('search_result.html', out_=out_)
 
-@app.route('/nothing_found')
+@app.route('/nothing_found', methods=['GET', 'POST'])
 def nothing_found():
-    with open('result.txt', 'r', encoding='utf-8') as file:
-        nothing = file.read()
-    out_ = nothing
-    return render_template('nothing_found.html', out_=out_)
+    return render_template('nothing_found.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
